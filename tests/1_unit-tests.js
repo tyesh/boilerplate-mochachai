46807854566e5b347c893ded09e130b5ca2e805a
unknown <carlos.velazquez91@gmail.com>
2022-11-02 10:09:45 -0300

    Task 1

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,8 +5,11 @@ suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
     // #1
     test('#isNull, #isNotNull', function () {
-      assert.fail(null, 'This is an optional error description - e.g. null is null');
-      assert.fail(1, '1 is not null');
+      assert.isNull(
+        null,
+        'This is an optional error description - e.g. null is null'
+      );
+      assert.IsNotNull(1, '1 is not null');
     });
     // #2
     test('#isDefined, #isUndefined', function () {
@@ -23,8 +26,14 @@ suite('Unit Tests', function () {
     // #4
     test('#isTrue, #isNotTrue', function () {
       assert.fail(true, 'true is true');
-      assert.fail(!!'double negation', 'Double negation of a truthy value is true');
-      assert.fail({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
+      assert.fail(
+        !!'double negation',
+        'Double negation of a truthy value is true'
+      );
+      assert.fail(
+        { value: 'truthy' },
+        'Objects are truthy, but are not boolean values'
+      );
     });
   });
 
@@ -47,8 +56,16 @@ suite('Unit Tests', function () {
     });
     // #7
     test('#deepEqual, #notDeepEqual', function () {
-      assert.fail({ a: '1', b: 5 }, { b: 5, a: '1' }, "The order of keys doesn't matter");
-      assert.fail({ a: [5, 6] }, { a: [6, 5] }, 'The order of array elements does matter');
+      assert.fail(
+        { a: '1', b: 5 },
+        { b: 5, a: '1' },
+        "The order of keys doesn't matter"
+      );
+      assert.fail(
+        { a: [5, 6] },
+        { a: [6, 5] },
+        'The order of array elements does matter'
+      );
     });
   });
 
@@ -87,7 +104,10 @@ suite('Unit Tests', function () {
   suite('Arrays', function () {
     // #11
     test('#isArray, #isNotArray', function () {
-      assert.fail('isThisAnArray?'.split(''), 'String.prototype.split() returns an array');
+      assert.fail(
+        'isThisAnArray?'.split(''),
+        'String.prototype.split() returns an array'
+      );
       assert.fail([1, 2, 3].indexOf(2), 'indexOf returns a number');
     });
     // #12
@@ -106,7 +126,10 @@ suite('Unit Tests', function () {
     // #13
     test('#isString, #isNotString', function () {
       assert.fail(Math.sin(Math.PI / 4), 'A float is not a string');
-      assert.fail(process.env.PATH, 'An env variable is a string (or undefined)');
+      assert.fail(
+        process.env.PATH,
+        'An env variable is a string (or undefined)'
+      );
       assert.fail(JSON.stringify({ type: 'object' }), 'JSON is a string');
     });
     // #14
